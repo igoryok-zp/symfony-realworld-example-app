5db42773cd16ef2912560b8ebddc4ca7491acfb8
Ihor Nakonechnyi <igoryok.zp@gmail.com>
2022-12-24 12:02:48 -0800

    Installed package jwt-auth

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
